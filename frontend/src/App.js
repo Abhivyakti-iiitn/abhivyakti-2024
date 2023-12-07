fd3b70ae7ddf6636e0cdafbe32a7b4f48ddfc92d
Adit Shriyans <shriyansadit@gmail.com>
2023-12-08 00:11:38 +0530

    basic done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,38 @@
-import "./css/index.css"
+import "./css/index.css";
+import EventCards from './EventCards';
+
+const events = [{name:'a', desc:"event a"},{ name:'b', desc: 'event b'},{ name:'c', desc: 'event c'},{ name:'d', desc:'event d'}, {name:'e f', desc:'event e f'}, {name:'Event', desc:'event Event'}];
+
+function animate() {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+        entry.target.classList.add('showCard');
+      }
+    });
+  });
+
+  const hiddenCards = document.querySelectorAll('.hiddenCard');
+  hiddenCards.forEach((el) => observer.observe(el));
+}
+
 function App() {
+  setTimeout(animate, 0);
+
   return (
     <div className="App">
-     Start Here!
+      <h1 className="Events__heading">Events</h1>
+      <div className="EventCards">
+        {
+          events.map((event) => 
+              <EventCards imgSrc = {`./assets/${event.name.replace(" ", "-")}.jpg`} name={event.name.replace(" ", "-")} desc={event.desc} key={event.name.replace(" ", "-")} />
+          )
+        }
+      </div>
     </div>
   );
 }
 
 export default App;
+
---
+++ frontend/src/assets/EventImages/EventCardsImageURL.js
@@ -0,0 +1,15 @@
+const ImageURLS = {
+    a: require('./a.jpg'),
+    b: require('./b.jpg'),
+    c: require('./c.jpg'),
+    d: require('./d.jpg'),
+    ef: require('./e-f.jpg')
+}
+
+// const ImageURLS = new Map(); 
+
+// ImageURLS.set('a', require('./a.jpg'));
+// ImageURLS.set('b', require('./b.jpg'));
+// ImageURLS.set('c', require('./c.jpg'));
+// ImageURLS.set('d', require('./d.jpg'));
+// ImageURLS.set('ef', require('./e-f.jpg'));
