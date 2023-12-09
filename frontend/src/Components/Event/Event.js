57cc9fe6d2ff29003489c95345f4d8d5196f5766
Supratit Datta <112554289+SupratitDatta@users.noreply.github.com>
2023-12-09 20:09:42 +0530

    added event page (#4)

---
+++ frontend/src/Components/Event/Event.js
@@ -0,0 +1,20 @@
+import './event.css'
+import Head from './Head'
+import Name from './Name'
+import Club from './Club'
+import Timeline from './Timeline'
+import Faq from './Faq'
+
+function Event() {
+    return (
+        <div className='container'>
+            <Head/>
+            <Name/>
+            <Club/>
+            <Timeline/>
+            <Faq/>
+        </div>
+    );
+}
+
+export default Event;
---
+++ frontend/src/Components/Event/Faq.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './faq.css';
+
+function Faq() {
+    return (
+        <div>
+            <div className='faq'>FREQUENTLY ASKED QUESTIONS</div>
+            <div className='linef1'></div>
+        </div>
+    );
+}
+
+export default Faq;
