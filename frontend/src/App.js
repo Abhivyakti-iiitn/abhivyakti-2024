a5d9b1259464f0b7899202a20531f96cf04e24ea
ShashankIIITN <shashanktripathi.st15a@gmail.com>
2023-12-09 21:47:43 +0530

    added route and bg asst

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,40 +2,37 @@ import "./css/index.css";
 import FirefliesAnimation from "./components/FirefliesAnimation"
 import EventCard from './components/EventCard';
 import Header from "./components/Header";
+import Event from "./components/EventPage/Event";
+import MainPage from "./components/MainPage";
+import { Routes, Route } from 'react-router-dom'
 
-const events = [{name:'a', desc:"event a"},{ name:'b', desc: 'event b'},{ name:'c', desc: 'event c'},{ name:'d', desc:'event d'}, {name:'e f', desc:'event e f'}];
 
-function animateEventCards() {
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      console.log(entry);
-      if (entry.isIntersecting) {
-        entry.target.classList.add('showCard');
-      }
-    });
-  });
+// const events = [{ name: 'a', desc: "event a" }, { name: 'b', desc: 'event b' }, { name: 'c', desc: 'event c' }, { name: 'd', desc: 'event d' }, { name: 'e f', desc: 'event e f' }];
 
-  const hiddenCards = document.querySelectorAll('.hiddenCard');
-  hiddenCards.forEach((el) => observer.observe(el));
-}
+// function animateEventCards() {
+//   const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//       console.log(entry);
+//       if (entry.isIntersecting) {
+//         entry.target.classList.add('showCard');
+//       }
+//     });
+//   });
+
+//   const hiddenCards = document.querySelectorAll('.hiddenCard');
+//   hiddenCards.forEach((el) => observer.observe(el));
+// }
 
 function App() {
 
-  setTimeout(animateEventCards, 0);
+  // setTimeout(animateEventCards, 0);
 
   return (
-    <div className="App">
-      {/* <FirefliesAnimation /> */}
-      <Header/>
-      <h1 className="Events__heading">Events</h1>
-      <div className="EventCards">
-        {
-          events.map((event) =>
-            <EventCard exploreAllEvents={false} imgSrc = {`./assets/${event.name.replace(" ", "-")}.jpg`} name={event.name.replace(" ", "-")} desc={event.desc} key={event.name.replace(" ", "-")} />
-          )
-        }
-        <EventCard exploreAllEvents={true} />
-      </div>
+    <div className="PApp">
+      <Routes>
+        <Route path="/" element={<MainPage />}></Route>
+        <Route path="/event" element={<Event />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/Components/EventPage/Timeline.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import '../../css/timeline.css';
+import e1 from '../../assets/EventPageAsst/E1.png'
+import e2 from '../../assets/EventPageAsst/E2.png'
+import mystery from '../../assets/EventPageAsst/mystery.png'
+
+function Timeline() {
+    return (
+        <div>
+            <div className='heading_t'>EVENT TIMELINE</div>
+            <div className='contain1'>
+                <div className='contain2'>
+                    <div className='line5'></div>
+                    <div className='contain3'>
+                        <div className='contain4'>
+                            <div className='c1'></div>
+                            <div className='v1'></div>
+                            <img src={e1} className='img1'></img>
+
+                        </div>
+                        <div className='contain5'>
+                            <div className='c2'></div>
+                            {/* <div className='v2'></div> */}
+                            <img src={e2} className='img2'></img>
+                        </div>
+                    </div>
+                </div>
+                <div className='diag1'></div>
+            </div>
+            <div className='contain6'>
+                <div className='guide'>
+                    <div>Round 1 (Eliminator) </div>
+                    <div>In this preliminary round, comedians will submit their recorded stand-up performances online. This serves as the eliminator, where your comedic creativity and talent will shine through the screen. Ensure that your submission embodies the theme and adheres to the rules, as it will determine your journey to the exciting final round. Get ready to bring the laughter to life in your quest for stardom at "Star-Pod.</div>
+                    <div className='rules'>RULES</div>
+                    <div>General Rules:</div>
+                    <div>1.  Comedy acts should be performed in Hindi, English, or a bilingual format.</div>
+                    <div>2. If selected for the final round, the same routine performed in the preliminary round may be delivered with slight alterations if desired.</div>
+                    <div>3. The use of any audio devices, except for the venue's sound system, is strictly prohibited.</div>
+                    <div>4. Comedians should adhere to the specified time limits for both preliminary and final rounds.</div>
+                    <div>5. Multiple comedians from the same institution are welcome to participate.</div>
+                    <div>6. Each comedian will perform individually.</div>
+                    <div>7. Please provide the name of the comedian.</div>
+                    <div>8. All performances must be original, and any content that is inappropriate, disrespectful, or offensive will not be tolerated.</div>
+                    <div>9. Routines should be performed live and not pre-recorded.</div>
+                </div>
+                <img src={mystery} className='mysteryimg'></img>
+            </div>
+            <div className='register'>REGISTER</div>
+
+            <div className='line7'></div>
+        </div>
+    );
+}
+
+export default Timeline;
