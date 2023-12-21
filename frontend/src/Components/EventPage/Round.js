041f5e701eb9e0b5fc74932300e757d02dda918e
Adit Shriyans <shriyansadit@gmail.com>
2023-12-22 01:11:35 +0530

    commit

--- frontend/src/Components/EventPage/Round.js
+++ frontend/src/Components/EventPage/Round.js
@@ -3,12 +3,12 @@ import React from 'react'
 const Round = (props) => {
   return (
     <div className='EventRound'>
-        <div className='top'>
-            <div className='diamond'></div>
-            <div className='EventDate'>{props.date}</div>
-        </div>
-        <div className='roundDetails'>
+        <div className='diamond'></div>
+        <div className='RoundContent'>
+        <div className='EventDate'>{props.date}</div>
+        <div className='RoundDetails'>
             ok
+        </div>
         </div>
     </div>
   )
