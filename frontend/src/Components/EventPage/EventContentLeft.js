41d90688457d3a3bb88c81ff1e23d433475f7beb
adimaniac <adityabhigade31@gmail.com>
2023-12-23 13:33:47 +0530

    styling for eventcontentleft

--- frontend/src/Components/EventPage/EventContentLeft.js
+++ frontend/src/Components/EventPage/EventContentLeft.js
@@ -5,23 +5,32 @@ const EventContentLeft = () => {
     <div className='EventContent-left'>
       <div className='sticky-content'>
         <div className='info-item'>
-          <img className='icon' src='/path/to/calendar-icon.png' alt='Calendar' />
-          Registration Deadline: <span>Date below it</span>
+          <img className='icon' src='/path/to/calendar-icon.png'/>
+          <div className='info'>
+          Registration Deadline: 
+          <span>Date below it</span>
+          </div>
         </div>
         <div className='info-item'>
-          <img className='icon' src='/path/to/users-icon.png' alt='Users' />
+          <img className='icon' src='/path/to/users-icon.png' />
+          <div className='info'>
           Team size: <span>No. below it</span>
+          </div>
         </div>
         <div className='info-item'>
-          <img className='icon' src='/path/to/money-icon.png' alt='Money' />
+          <img className='icon' src='/path/to/money-icon.png'/>
+          <div className='info'>
           Registration Fee: <span>10 rs</span>
+          </div>
         </div>
         <div className='info-item'>
-          <img className='icon' src='/path/to/trophy-icon.png' alt='Trophy' />
+          <img className='icon' src='/path/to/trophy-icon.png'/>
+          <div className='info'>
           Prize Pool: <span>Prize pool</span>
+          </div>
         </div>
         <button className='register-button'>
-          Register
+          Register Now
         </button>
       </div>
     </div>
