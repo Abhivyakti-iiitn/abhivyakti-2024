f46f976dcd394118ecefb2ce6756d57a26937804
Adit Shriyans <shriyansadit@gmail.com>
2023-12-23 21:46:39 +0530

    ok

--- frontend/src/Components/EventPage/EventContentRight.js
+++ frontend/src/Components/EventPage/EventContentRight.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Round from './Round'
 import Faq from './Faq'
 import Head from './Head'
 
 const EventContentRight = (props) => {
+  const [alreadyGlowing, setAlreadyGlowing] = useState(false);  
+
+  const handleGlowing = () => {
+    if (!alreadyGlowing) {
+      setAlreadyGlowing(true);
+      console.log("ret true");
+      return true;
+    }
+    console.log("ret false");
+    return false;
+  };
+  
   return (
     <div className='EventContent-right'>
-      <div className='details'>
-        <div className='EventContent__heading'>
+      <div className='EventDetail details'>
+        <div className='EventContent_heading'>
           <fieldset>
             <legend>
               Details
@@ -18,8 +30,8 @@ const EventContentRight = (props) => {
           {props.details}
         </div>
       </div>
-      <div className='roundsAndTimline'>
-        <div className='EventContent__heading'>
+      <div className='EventDetail roundsAndTimline'>
+        <div className='EventContent_heading'>
           <fieldset>
             <legend>
               Rounds & Timeline
@@ -27,13 +39,25 @@ const EventContentRight = (props) => {
           </fieldset>
         </div>
         <div className='EventContent_content'>
-          {props.timeline.map((date) => (
-            <Round key={date} date={date} />
-          ))}
+          {props.timeline.map((date, id) => {
+            const isLastElement = id === props.timeline.length - 1;
+
+            const parts = date.split('/');
+            const eventDate = new Date(`20${parts[2]}`, parts[1] - 1, parts[0]);
+            const today = new Date();
+            today.setHours(0, 0, 0, 0);
+            console.log("check"+alreadyGlowing);
+            const glow = today <= eventDate && handleGlowing();
+            console.log(glow);
+
+            return (
+              <Round key={id} date={date} isLastTimeline={isLastElement} glow={glow} />
+            )
+          })}
         </div>
       </div>
-      <div className='rulesAndRegulation'>
-        <div className='EventContent__heading'>
+      <div className='EventDetail rulesAndRegulation'>
+        <div className='EventContent_heading'>
           <fieldset>
             <legend>
               Rules & Regulations
