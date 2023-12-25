0b3e21267a9dc4bf343e0875e5e05c807e060013
adimaniac <adityabhigade31@gmail.com>
2023-12-25 23:30:13 +0530

    create formPageLeft

--- frontend/src/Components/RegistrationForm/FormPage.js
+++ frontend/src/Components/RegistrationForm/FormPage.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import EventContent from "../../assets/EventContent.json";
+import React from 'react'
+import FormPageLeft from './FormPageLeft'
+import Form from './Form'
 
 const FormPage = () => {
   return (
-    <div>
-        <h1>EventContent.showStopper.name</h1>
+    <div className='FormPage'>
+        <FormPageLeft />
+        <Form />
     </div>
   )
 }
