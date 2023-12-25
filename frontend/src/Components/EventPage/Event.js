5be49441cedd4b9c2e9810819984f1b16bfd4be7
ShashankIIITN <shashanktripathi.st15a@gmail.com>
2023-12-26 01:03:35 +0530

    fixed event page

--- frontend/src/Components/EventPage/Event.js
+++ frontend/src/Components/EventPage/Event.js
@@ -1,4 +1,5 @@
 import '../../css/event.css';
+import { useEffect } from 'react';
 import Head from './Head'
 import Name from './Name'
 import Club from './Club'
@@ -10,30 +11,58 @@ import Contact from './Contact';
 import FAQ from './Faq';
 import content from '../../assets/EventContent.json';
 import Header from './Header';
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Footer from './Footer';
 
 // const content={name:"showstopper", details:"Join us for a dance journey where every step is a brushstroke on the canvas of your soul, where the stage is your sanctuary of expression, and where you dance not just with your body, but with your heart and spirit. Don't miss this opportunity to shine and be part of the vibrant cultural tapestry of \"Abhivyakti.\"", timeline:["15/01/24", "4/02/24", "10/02/24", "10/02/24", "15/02/24"], rules:"", regulations:"chipi chipi chapa chapa dubi dubi daba daba",registrationDate:"15/1/24",Team:"4",fees:"10 Rs",pool:"69 Cr"}
 
 function Event() {
+
+    function fx() {
+        let header = document.querySelector(".event_header");
+        let header2 = document.querySelector(".sticky_header");
+        let cont = document.querySelector(".container");
+        if (cont.scrollTop > header.clientHeight - (header.clientHeight * 20 / 100) && !header2.classList.contains('shown')) {
+
+            // console.log(header.clientHeight - (header.clientHeight * 20 / 100))
+            // console.log(header.classList)
+            header2.classList.add("shown");
+        }else if(cont.scrollTop < header.clientHeight - (header.clientHeight * 20 / 100) && header2.classList.contains('shown')){
+            
+            header2.classList.remove("shown");
+        }
+
+
+        // document.querySelector(".container").classList.toggle("sticky");
+
+    }
+
+    useEffect(() => {
+
+        const com = document.querySelector(".container");
+        com.addEventListener("scroll", fx);
+
+    }, [])
+
+
     const params = useParams();
-    console.log(params);
-    console.log(content);
+    // console.log(params);
+    // console.log(content);
     return (
         <div className='container'>
-            <Header event = {content[params.eventname]}/>
+            <Header event={content[params.eventname]} />
             {/* <Head/>
             <Name/>
             <Club/>
             <Timeline/>
             <Faq/> */}
             <EventContent content={content[params.eventname]} />
-            <Prize/>
+            <Prize />
             <Faq />
             <Contact />
 
-            <Footer/>
-            
+            <Footer />
+
         </div>
     );
 }
