fde449c28cf80ad8834ddefdb9b7cb17796e6acc
Adit Shriyans <shriyansadit@gmail.com>
2023-12-27 01:13:26 +0530

    add underline animation

--- frontend/src/Components/MainPage.js
+++ frontend/src/Components/MainPage.js
@@ -9,7 +9,6 @@ import Event from "./EventPage/Event";
 import HorizontalScrolling from "./HorizontalScrolling";
 import Sponsors from "./Sponsors"; // Import the Sponsors component
 import Footer from "./Footer"
-import AllEvents from "./AllEvents";
 const events = [
   { name: "showstopper", desc: "event a" },
   { name: "beatsmithShowdown", desc: "event b" },
@@ -49,7 +48,6 @@ function MainPage() {
     <div className="App">
       {/* <FirefliesAnimation /> */}
       <Header />
-      <AllEvents />
       <h2 className="Events__heading">Events</h2>
       <div className="EventCards">
         {events.map((event) => (
