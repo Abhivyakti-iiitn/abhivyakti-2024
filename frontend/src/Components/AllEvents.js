1689b079bdefd0187220a8b640227c0b44b9a98b
Adit Shriyans <shriyansadit@gmail.com>
2023-12-26 22:54:35 +0530

    add all events

---
+++ frontend/src/Components/AllEvents.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import eventsArray from "../assets/EventDetails.json";
+import '../css/AllEvents.css';
+import EventSlide from './EventSlide';
+
+const AllEvents = () => {
+    const [eventType, setEventType] = useState('');
+    const [activeButton, setActiveButton] = useState('all'); // Initially set to 'all'
+
+    const filteredEvents = eventType === "Solo" ? eventsArray.filter(event => event.Team === "Solo") : eventsArray.filter(event => event.Team !== "Solo");
+    const events = eventType ? filteredEvents : eventsArray;
+
+    return (
+        <div className='AllEvents'>
+            <div className='AllEvents__heading'>
+                <fieldset className='AllEventsFS'>
+                    <legend className='AllEventsL'>
+                        All Events
+                    </legend>
+                </fieldset>
+            </div>
+            <div className='EventTypeControl'>
+                <div
+                    key='all'
+                    className={`EventTypeBtn ${activeButton === 'all' ? 'active' : ''}`}
+                    onClick={() => {
+                        setEventType('');
+                        setActiveButton('all');
+                    }}
+                >
+                    All Events
+                    <div className='AllEventsUnderline'></div>
+                </div>
+                <div
+                    key='solo'
+                    className={`EventTypeBtn ${activeButton === 'solo' ? 'active' : ''}`}
+                    onClick={() => {
+                        setEventType('Solo');
+                        setActiveButton('solo');
+                    }}
+                >
+                    Solo Events
+                    <div className='AllEventsUnderline'></div>
+                </div>
+                <div
+                    key='team'
+                    className={`EventTypeBtn ${activeButton === 'team' ? 'active' : ''}`}
+                    onClick={() => {
+                        setEventType('Team');
+                        setActiveButton('team');
+                    }}
+                >
+                    Team Events
+                    <div key={'underline'} className='AllEventsUnderline'></div>
+                </div>
+            </div>
+            <div className='EventSlides'>
+                {events.map((event) => (
+                    <div key={event.id}>
+                        <EventSlide event={event} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default AllEvents;
---
+++ frontend/src/Components/EventSlide.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import showstopperImgSrc from '../assets/EventPageAsst/showstopper.png'
+import starpodImgSrc from '../assets/EventPageAsst/starpod.png'
+import beatsmithshowdownImgSrc from '../assets/EventPageAsst/beatboxshowdown.png'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { useNavigate } from 'react-router-dom';
+
+const eventImages = {
+    showstopper: showstopperImgSrc,
+    starpod: starpodImgSrc,
+    beatsmithShowdown: beatsmithshowdownImgSrc,
+};
+
+const EventSlide = (props) => {
+    const nav = useNavigate();
+    const imgSrc = eventImages[props.event.name] || '';
+
+    const contentStyle = {
+        backgroundImage: `url(${imgSrc})`,
+        width: '100%',
+        backgroundPosition: 'center',
+    };
+
+    return (
+        <div className='EventSlide'>
+            <div className='EventSlide__Img'>
+                <img className='EventImage' src={imgSrc} />
+            </div>
+            <div className='EventSlide__Content' style={contentStyle}>
+                <div className='EventSlide__text'>
+                    <div className='EventSlide__name'>{props.event.name}</div>
+                    <div className='EventSlide__desc'>Description</div>
+                    <div className='EventSlide__learnmore' onClick={()=>{nav(`/event/${props.event.name}`)}}>Learn More <ArrowForwardIcon className='EventSlide__learnmore-Arrow' fontSize='small' /> </div>
+                </div>
+                <div className='EventSlide__btn'>
+                    <button className='EventRegBtn'>
+                        Register
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default EventSlide
