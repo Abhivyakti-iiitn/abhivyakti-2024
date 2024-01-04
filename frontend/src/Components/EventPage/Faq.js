49881b1a6093002a23ca042889bdc8b991636d54
Adit Shriyans <shriyansadit@gmail.com>
2024-01-04 23:06:37 +0530

    faq style

--- frontend/src/Components/EventPage/Faq.js
+++ frontend/src/Components/EventPage/Faq.js
@@ -1,5 +1,6 @@
 import faqData from '../../assets/faqData';
 import "../../css/faq.css";
+import '../../css/faqNew.css';
 import React, { useState } from 'react';
 import { useSpring, animated } from 'react-spring';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
@@ -10,12 +11,13 @@ const Faq = () => {
     const [activeIndex, setActiveIndex] = useState(null);
 
     const toggleSection = (sectionIndex) => {
+        console.log(sectionIndex);
         setActiveSection(activeSection === sectionIndex ? null : sectionIndex);
-        setActiveIndex(null); // Reset activeIndex when changing sections
     };
 
-    const toggleAnswer = (index) => {
-        setActiveIndex(index === activeIndex ? null : index);
+    const toggleAnswer = (sectionIndex, index) => {
+        setActiveSection(sectionIndex);
+        setActiveIndex(activeIndex !== null ? null : index);
     };
 
     const fadeIn = useSpring({
@@ -25,55 +27,56 @@ const Faq = () => {
     return (
         <div className="faq-container">
             <div className="liner">
-
                 <div className="line"></div>
                 <h1>FAQ</h1>
                 <div className="line"></div>
             </div>
-            {/* {faqData.map((faq, index) => (
-                <div key={index} className="faq-item">
-                    <div className={`faq-question ${activeIndex === index ? 'active' : ''}`}>
-                        <div className="faq-header" onClick={() => toggleAnswer(index)}>
-                            {faq.question}
-                            <span
-                                className="faq-toggle-button"
-                            >
-                                {activeIndex === index ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
-                            </span>
-                        </div>
-                        <animated.div style={fadeIn} className={`faq-answer ${activeIndex === index ? 'show' : ''}`}>
-                            {faq.answer}
-                        </animated.div>
-                    </div>
-                </div>
-            ))} */}
-            <div className="faq-list">
-                {Object.entries(faqData).map(([section, faqs], sectionIndex) => (
-                    <div key={sectionIndex} className="faq-section">
-                        <div className={`faq-section-header ${activeSection === sectionIndex ? 'active' : ''}`} onClick={() => toggleSection(sectionIndex)}>
-                            {section}
-                        </div>
-                        {activeSection === sectionIndex && (
-                            <div className="faq-section-content">
-                                {faqs.map((faq, index) => (
-                                    <div key={index} className="faq-item">
-                                        <div className={`faq-question ${activeIndex === index ? 'active' : ''}`} onClick={() => toggleAnswer(index)}>
-                                            {faq.question}
-                                            <span className="faq-toggle-button">
-                                                {activeIndex === index ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
-                                            </span>
-                                        </div>
-                                        <animated.div style={fadeIn} className={`faq-answer ${activeIndex === index ? 'show' : ''}`}>
-                                            {faq.answer}
-                                        </animated.div>
+            <div className='faq-main'>
+                {activeSection!==null ?
+                    (Object.keys(faqData).map((category, categoryIndex) => {
+                        if (categoryIndex === activeSection) {
+                            return (
+                                <div key={categoryIndex} className="faq-category">
+                                    <div className='faq-categorycontainer'>
+                                        <h2 className='faq-categoryName faq-question' >{category}</h2>
+                                        <i onClick={() => toggleSection(categoryIndex)} className='fas fa-arrow-left'></i>
                                     </div>
-                                ))}
-                            </div>
-                        )}
-                    </div>
-                ))}
+                                    {faqData[category].map((faq, index) => (
+                                        <div key={index} className="faq-item">
+                                            <div className={`faq-question ${activeIndex === index ? 'active' : ''}`}>
+                                                <div className="faq-header" onClick={() => toggleAnswer(categoryIndex, index)}>
+                                                    {faq.question}
+                                                    <span className="faq-toggle-button">
+                                                        {activeIndex === index ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
+                                                    </span>
+                                                </div>
+                                                <animated.div style={fadeIn} className={`faq-answer ${activeIndex === index && activeSection === categoryIndex ? 'show' : ''}`}>
+                                                    {faq.answer}
+                                                </animated.div>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            );
+                        }
+                        return null;
+                    })) :
+                    (
+                        <div className='faq-sections'>
+                            {Object.keys(faqData).map((category, categoryIndex) => (
+                                <div key={categoryIndex} className='faq-item faq-section' onClick={() => toggleSection(categoryIndex)}>
+                                    <h2 key={categoryIndex} 
+                                        className='faq-categoryHeading faq-question faq-header'
+                                    >
+                                        {category}
+                                    </h2>
+                                    <i key={categoryIndex} className='fas fa-arrow-right'></i>
+                                </div>
+                            ))}
+                        </div>
+                    )
+                }
             </div>
-
         </div>
     );
 };
