860456ede87a7d72cad173dc10faef32a7998f17
ShashankIIITN <shashanktripathi.st15a@gmail.com>
2024-01-08 22:28:43 +0530

    updated schema

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,34 +1,36 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-import { errorHandler } from "../utils/error.js";
+// import { errorHandler } from "../utils/error.js";
 import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
 
-export const signup = async (req,res,next) =>{
-    const {username,email,password} = req.body;
-    const hashedPassword = bcryptjs.hashSync(password,10);
-    const newUser = new User({ username, email, password: hashedPassword });
-    try{
-    await newUser.save();
-    res.status(201).json(newUser);
+export const signup = async (req, res, next) => {
+    const { username, email, password } = req.body;
+
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword, emailToken: crypto.randomBytes(64).toString("hex") });
+    try {
+        await newUser.save();
+        res.status(201).json(newUser);
     }
-    catch(err){
+    catch (err) {
         next(err);
     }
 };
 
 
-export const signin = async (req,res,next) => {
-   const {email,password} =req.body;
-   try{
-        const validUser = await User.findOne({email});
-        if(!validUser) return next(errorHandler(404,'User Not Found!'));
-        const validPassword= bcryptjs.compareSync(password,validUser.password);
-        if(!validPassword) return next(errorHandler(401,'Wrong Credentials!'));
-        const token = jwt.sign({id:validUser.id}, process.env.JWT_SECRET);
-        const {password:pass,...rest} = validUser._doc;
-        res.cookie('access_token',token,{httpOnly:true}).status(200).json({rest});
-   }
-   catch(error){
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User Not Found!'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Wrong Credentials!'));
+        const token = jwt.sign({ id: validUser.id }, process.env.JWT_SECRET);
+        const { password: pass, ...rest } = validUser._doc;
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json({ rest, access_token: token });
+    }
+    catch (error) {
         next(error);
-   }
+    }
 };
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,22 +1,30 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
+    username: {
+        type: String,
         required: true,
         unique: true,
     },
-    password:{
+    password: {
         type: String,
         required: true
     },
-    email:{
-        type:String,
+    email: {
+        type: String,
         required: true,
         unique: true,
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    emailToken: {
+        type: String
     }
-},   {timestamps: true});
 
-const User = mongoose.model('User',userSchema);
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
 
 export default User;
