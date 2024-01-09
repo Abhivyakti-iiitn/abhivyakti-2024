efa054f1ebe3f9605893c42e058c7875f28022b6
Harsh kasana <bt22ece106@gmail.com>
2024-01-09 21:50:30 +0530

    Added code for token

---
+++ backend/middleware/error.js
@@ -0,0 +1,24 @@
+const ErrorHandler = require("../utils/errorhander");
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+
+  
+  // Wrong JWT error
+  if (err.name === "JsonWebTokenError") {
+    const message = `Json Web Token is invalid, Try again `;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // JWT EXPIRE error
+  if (err.name === "TokenExpiredError") {
+    const message = `Json Web Token is Expired, Try again `;
+    err = new ErrorHandler(message, 400);
+  }
+
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
