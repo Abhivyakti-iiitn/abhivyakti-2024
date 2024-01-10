fda7970dc7f345ba33cec0b887c86d959310166b
Adit Shriyans <shriyansadit@gmail.com>
2024-01-10 21:15:43 +0530

    modal styles and gh links

--- frontend/src/Components/Modal.js
+++ frontend/src/Components/Modal.js
@@ -3,37 +3,13 @@ import ReactDom from 'react-dom'
 import "../css/Modal.css"
 import qr from "../assets/qr.jpg"
 
-// const MODAL_STYLES = {
-//     position: 'fixed',
-//     top: '50%',
-//     left: '50%',
-//     transform: 'translate(-50%, -50%)',
-//     backgroundColor: '#FFF',
-//     padding: '50px',
-//     zIndex: 1000
-// }
-
-// const OVERLAY_STYLES = {
-//     position: 'fixed',
-//     top: 0,
-//     left: 0,
-//     right: 0,
-//     bottom: 0,
-//     backgroundColor: 'rgba(0, 0, 0, .7)',
-//     zIndex: 1000
-// }
-
 export default function Modal({ open, onClose, handleSubmit, formData, handleChange }) {
     if (!open) return null
 
     return ReactDom.createPortal(
         <>
-            <div 
-            // style={OVERLAY_STYLES} 
-            className='Modal__overlay' />
-            <div 
-            // style={MODAL_STYLES} 
-            className='Modal'>
+            <div className='Modal__overlay' />
+            <div className='Modal'>
                 <button className='Modal__btn' onClick={onClose}>X</button>
                 <h3 className='Modal__text'>UPI-7058449059@axl</h3>
                 <img src={qr} alt="7058449059@axl" className="qr Modal__qr" />
