80b5c9fee4055af3a0ae4c2cd2f830de5e844fdc
Supratit Datta <supratitdatta@gmail.com>
2024-01-12 14:05:45 +0530

    added profile pic

--- frontend/src/Components/Profile.js
+++ frontend/src/Components/Profile.js
@@ -21,6 +21,7 @@ import groovegenesis from '../assets/EventPageAsst/Groove Genesis(Instrumental).
 import mun from '../assets/EventPageAsst/munmain.png';
 import roadtoredcarpet from '../assets/EventPageAsst/Road to red carpet.png';
 import defaultImg from '../assets/EventImages/a.jpg';
+import profilepic from "../assets/ProfilePage/profilepic.png"
 
 
 function Profile() {
@@ -127,7 +128,7 @@ function Profile() {
                 <div className='main_container'>
                     <div className="user_details_head">DETAILS</div>
                     <div className='user_box'>
-                        <img className='profile_pic' src='https://www.asianpaints.com/content/dam/asian_paints/colours/swatches/L161.png.transform/cc-width-720-height-540/image.png' alt=''></img>
+                        <img className='profile_pic' src={profilepic} alt=''></img>
                         <div className='user_details'>
                             <div>
                                 <div className='user_info user_name'>{context.userData?.username}</div>
@@ -171,7 +172,7 @@ function Profile() {
                 <div className='main_container2'>
                     <div className="user_details_head ush2">DETAILS</div>
                     <div className='user_box2'>
-                        <img alt='' className='profile_pic2' src='https://www.asianpaints.com/content/dam/asian_paints/colours/swatches/L161.png.transform/cc-width-720-height-540/image.png'></img>
+                        <img alt='' className='profile_pic2' src={profilepic}></img>
                         <div className='user_details2'>
                             <div className='user_name'>{context.userData?.username}</div>
                             <div className='det'>{context.userData?.college}</div>
