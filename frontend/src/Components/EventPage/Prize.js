18f08f9b7d67f4a9eb9a066280105d3098201de8
Adit Shriyans <shriyansadit@gmail.com>
2024-01-26 15:16:45 +0530

    commit

--- frontend/src/Components/EventPage/Prize.js
+++ frontend/src/Components/EventPage/Prize.js
@@ -14,18 +14,36 @@ function Prize(props) {
                 <div className="line"></div>
             </div>
             <div className='prize_amount'>
-                {props.prize?.length > 1 && <div>
-                    <div>Second Prize</div>
-                    <div>{props.prize[1]}</div>
-                </div>}
-                {props.prize?.length > 0 && <div>
-                    <div>{isModelUnitedNations ? 'Prize Pool':'First Prize'}</div>
+                {props.prize?.length === 1 && <div>
+                    <div>{isModelUnitedNations ? 'Prize Pool' : 'First Prize'}</div>
                     <div>{props.prize[0]}</div>
                 </div>}
-                {props.prize?.length === 3 && <div>
-                    <div>Third Prize</div>
-                    <div>{props.prize[2]}</div>
-                </div>}
+                
+                {props.prize?.length === 2 && <>
+                    <div>
+                        <div>{isModelUnitedNations ? 'Prize Pool' : 'First Prize'}</div>
+                        <div>{props.prize[0]}</div>
+                    </div>
+                    <div>
+                        <div>Second Prize</div>
+                        <div>{props.prize[1]}</div>
+                    </div>
+                </>}
+
+                {props.prize?.length === 3 && <>
+                    <div>
+                        <div>Second Prize</div>
+                        <div>{props.prize[1]}</div>
+                    </div>
+                    <div>
+                        <div>{isModelUnitedNations ? 'Prize Pool' : 'First Prize'}</div>
+                        <div>{props.prize[0]}</div>
+                    </div>
+                    <div>
+                        <div>Third Prize</div>
+                        <div>{props.prize[2]}</div>
+                    </div>
+                </>}
             </div>
             <div className='register_btn' onClick={() => navigateTo(`/form/${props.name?.toLowerCase().replace(' ', '')}`)}>Register Now</div>
         </div>
